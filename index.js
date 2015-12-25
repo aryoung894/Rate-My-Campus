28234fed0876b45ffda65c1dead50bfc5bd2befe
Steven Lim <stevenlim890@gmail.com>
2015-12-25 01:26:25 -0800

    onto styling

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(session({
 	resave: false,
   	saveUninitialized: true
 }));
+app.use(function(req, res, next) { res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'); next(); });
 
 app.get('/', getRoute.loginpage);
 app.get('/signup', getRoute.signUpPage);
